3ff95ce00d9209cb8e021ac2feccbb48da93d0b0
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2024-12-05 14:09:08 -0300

    Update principal.java

--- revisao_prog2/src/revisao_prog2/principal.java
+++ revisao_prog2/src/revisao_prog2/principal.java
@@ -16,29 +16,32 @@ public class principal {
 //		System.out.println("Soma dos ímpares = " + soma);
 //		System.out.println("Multiplicação dos pares = " + mult);
 		
-		int a = 2, b = 99, aux;
-		System.out.println("Antes");
-		System.out.println("a = " + a + " | b = " + b);
+		// int a = 2, b = 99, aux;
+		// System.out.println("Antes");
+		// System.out.println("a = " + a + " | b = " + b);
 		
 		
-		a = b - a;
-		System.out.println("no primeiro passo o a esta " + a + " e o b esta como " + b);
-		b = b - a;
-		System.out.println("no segundo passo o a esta " + a + " e o b esta como " + b);
+		// a = b - a;
+		// System.out.println("no primeiro passo o a esta " + a + " e o b esta como " + b);
+		// b = b - a;
+		// System.out.println("no segundo passo o a esta " + a + " e o b esta como " + b);
 
-		a = b + a;
-		System.out.println("no terceito passo o a esta " + a + " e o b esta como " + b);
+		// a = b + a;
+		// System.out.println("no terceito passo o a esta " + a + " e o b esta como " + b);
 
-		System.out.println("outro Depois");
-		System.out.println("a = " + a + " | b = " + b);
+		// System.out.println("outro Depois");
+		// System.out.println("a = " + a + " | b = " + b);
 		
 		
-		aux = a;
-		a = b;
-		b = aux;
-		System.out.println("Depois");
-		System.out.println("a = " + a + " | b = " + b);
-		
+		// aux = a;
+		// a = b;
+		// b = aux;
+		// System.out.println("Depois");
+		// System.out.println("a = " + a + " | b = " + b);
+		agenda agenda = new Agenda();
+		agenda.addContato(new Contato("Reginaldo", "regicsf@ufpa.br"));
+		agenda.addContato(new Contato("Luciana"), "luciana@ufpa.br");
+		agenda.addContato(new Contato("Carlos", "carlos@ufpa.br"));
 
 		
 	}
