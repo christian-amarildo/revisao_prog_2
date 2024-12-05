81153d1adc29cd702bdc034b527a8eabb2594420
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2024-12-05 14:08:04 -0300

    Update Contato.java

--- revisao_prog2/src/revisao_prog2/Contato.java
+++ revisao_prog2/src/revisao_prog2/Contato.java
@@ -1,5 +1,23 @@
 package revisao_prog2;
 
 public class Contato {
-
+	private String nome;
+	private String email;
+	
+	public Contato(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	public String getNome () {
+		return this.nome;
+	}
+	
+	public void setNome (String nome) {
+		this.nome = nome;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
